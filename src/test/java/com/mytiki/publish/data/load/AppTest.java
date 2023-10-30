cdb418733ecde57ff9faf28884fe35911a496635
Mike Audi <mike@mytiki.com>
2023-10-29 23:11:03 -0500

    fix: tests

--- src/test/java/com/mytiki/publish/data/load/AppTest.java
+++ src/test/java/com/mytiki/publish/data/load/AppTest.java
@@ -7,40 +7,39 @@ package com.mytiki.publish.data.load;
 
 import com.amazonaws.services.lambda.runtime.events.SQSBatchResponse;
 import com.amazonaws.services.lambda.runtime.events.SQSEvent;
+import com.amazonaws.services.lambda.runtime.events.models.s3.S3EventNotification;
+import com.amazonaws.services.lambda.runtime.serialization.PojoSerializer;
+import com.amazonaws.services.lambda.runtime.serialization.events.LambdaEventSerializers;
 import com.amazonaws.services.lambda.runtime.tests.annotations.Event;
-import com.mytiki.publish.data.load.AvroToParquet;
-import com.mytiki.publish.data.load.WriteHandler;
+import com.mytiki.publish.data.load.file.FileClient;
 import com.mytiki.publish.data.load.file.FileMetadata;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.io.IOException;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
-@ExtendWith(MockitoExtension.class)
 public class AppTest {
 
-    AvroToParquet atp;
+    FileClient client;
+    final PojoSerializer<S3EventNotification> serializer =
+            LambdaEventSerializers.serializerFor(S3EventNotification.class, ClassLoader.getSystemClassLoader());
 
     @BeforeEach
-    void init(@Mock AvroToParquet atp) throws IOException {
-        this.atp = atp;
-        Mockito.lenient().doReturn(List.of()).when(atp).read(Mockito.anyString(), Mockito.anyString());
-        Mockito.lenient().doReturn(new FileMetadata()).when(atp).write(Mockito.anyString(), Mockito.anyList());
-        Mockito.lenient().doReturn("dummy").when(atp).notify(Mockito.any());
+    void init() throws IOException {
+        this.client = Mockito.mock(FileClient.class);
+        Mockito.lenient().doReturn(List.of()).when(client).read(Mockito.anyString());
+        Mockito.lenient().doReturn("dummy").when(client).write(Mockito.anyString(), Mockito.anyList());
     }
 
     @ParameterizedTest
     @Event(value = "sqs-event.json", type = SQSEvent.class)
     public void event(SQSEvent event) {
-        WriteHandler handler = new WriteHandler(atp);
+        Handler handler = new Handler(client, serializer);
         SQSBatchResponse rsp = handler.handleRequest(event, null);
         assertEquals(0, rsp.getBatchItemFailures().size());
     }
